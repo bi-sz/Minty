8658d8834ca4f3d700ff0f7f54e9922682bf8625
bi <kwonbe99@gmail.com>
2023-06-30 17:21:39 +0900

    0630 병합 포인트샵, 찜하기 추가

---
+++ src/main/reactview/src/component/boardDetail.js
@@ -0,0 +1,217 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Button, Carousel, Stack, Modal } from 'react-bootstrap';
+import '../css/boardDetail.css';
+import { formatDistanceToNow, parseISO } from 'date-fns';
+import { ko } from 'date-fns/locale';
+
+
+function BoardDetail({ csrfToken }) {
+  let [tradeBoard, setTradeBoard] = useState({});
+    const [isAuthor, setIsAuthor] = useState(false);
+
+  let [imageList, setImageList] = useState([]);
+  let [currentImage, setCurrentImage] = useState(0);
+
+  const [nickName, setNickName] = useState('');
+  const [showModal, setShowModal] = useState(false); // Modal 표시 여부 상태
+  const [popupMessage, setPopupMessage] = useState('');
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+   const handleEditClick = () => {
+     navigate(`/writeForm/${id}`, { state: { tradeBoard, imageList } });
+   };
+
+const fetchData = () => {
+  axios
+    .get(`/api/boardDetail/${id}`)
+    .then((response) => {
+      if (response.status === 200) {
+        setTradeBoard(response.data.tradeBoard);
+        let list = [...response.data.imageList];
+        setNickName(response.data.nickName);
+        setImageList(list);
+        setIsAuthor(response.data.author);
+      } else {
+        alert("알 수 없는 오류");
+        window.history.back(); // 이전 페이지로 이동
+      }
+    })
+    .catch((error) => {
+      if (error.response && error.response.status) {
+        const statusCode = error.response.status;
+        if (statusCode === 403) {
+          alert(error.response.data);
+        } else if (statusCode === 404) {
+          alert(error.response.data);
+        } else {
+          alert(error.response.data);
+        }
+      } else {
+        console.log(error);
+        alert("error");
+      }
+      window.history.back(); // 이전 페이지로 이동
+    });
+};
+
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  let timeAgo = '';
+  if (tradeBoard.createdDate) {
+    timeAgo = formatDistanceToNow(parseISO(tradeBoard.createdDate), { addSuffix: true, locale: ko });
+  }
+
+  const handleImageClick = (index) => {
+    setCurrentImage(index);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const incrementInterestingCount = () => {
+      axios
+        .post(`/api/incrementInterestingCount/${id}`, tradeBoard.id,{
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken,
+          },
+        })
+        .then((response) => {
+          // Set the popup message
+          alert('관심상품에 등록되었습니다.');
+          console.log('카운트 증가됨:', response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          alert('카운트 증가 오류');
+        });
+    };
+
+  const purchasingReq = () => {
+      axios
+        .post('/api/purchasingReq', tradeBoard.id, {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken,
+          },
+        })
+        .then((response) => {
+          window.location.href = response.data;
+        })
+        .catch((error) => {
+             if (error.response && error.response.status === 400) {
+                      alert("이미 존재하는 구매 요청입니다.");
+                      window.location.href = error.response.data;
+             } else {
+               // Other errors - Show generic error message in alert
+               console.log(error);
+               alert('An error occurred.');
+             }
+           });
+    };
+
+ const handleDeleteClick = () => {
+   axios
+          .post('/api/tradeBoard/deleteRequest', tradeBoard.id, {
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-TOKEN': csrfToken,
+            },
+          }).then((response)=> {
+              alert('삭제 처리 되었습니다.');
+              window.location.href = "/boardList/";
+          })
+           .catch((error) => {
+                  console.log(error);
+                  alert(error);
+                })
+          };
+
+
+  return (
+    <Container>
+      <Row className="board-top">
+        <Col className="overflow-container img-container">
+          <Carousel
+            variant={'dark'}
+            interval={null}
+            slide={false}
+            activeIndex={currentImage}
+            onSelect={(index) => setCurrentImage(index)}
+          >
+            {imageList.map((img, index) => (
+              <Carousel.Item key={img.id}>
+                <img
+                  src={`https://storage.cloud.google.com/reboot-minty-storage/${img.imgUrl}`}
+                  alt="Board Image"
+                  className="board-img"
+                  onClick={() => handleImageClick(index)} // 이미지 클릭 이벤트 처리
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Col>
+        <Col>
+          <Stack gap={3}>
+            <h2>{tradeBoard.title}</h2>
+            <h2>{Number(tradeBoard.price).toLocaleString()} 원</h2>
+            <h2>{nickName}</h2>
+          </Stack>
+          <Col className="board-stats">
+            <span>🤍 {tradeBoard.interesting}</span>
+            <span>👁‍ {tradeBoard.visit_count}</span>
+            <span>{timeAgo}</span>
+          </Col>
+          <Col className="button-groups">
+            {!isAuthor &&<Button variant="primary" onClick={incrementInterestingCount}>찜하기</Button>}
+            {!isAuthor &&<Button variant="secondary">채팅</Button>}
+            {!isAuthor &&<Button variant="success" onClick={purchasingReq}>
+              구매 신청
+            </Button>}
+          </Col>
+        </Col>
+      </Row>
+      <br />
+      <br />
+      <Row className="board-content">
+        <Col style={{ whiteSpace: "pre-wrap" }}>{tradeBoard.content}</Col>
+      </Row>
+      <br/><br/>
+      <Row>
+        <Col md={12}></Col>
+       <Col md={3}>
+       <div>
+       <Row className="justify-content-end">
+        <Col>
+            {isAuthor && <Button variant="primary" onClick={handleEditClick} style={{gap:"3"}}>수정</Button>}
+            {isAuthor && <Button variant="primary" onClick={handleDeleteClick}>삭제</Button>}
+        </Col>
+       </Row>
+       </div>
+       </Col>
+      </Row>
+
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Body className="modal-body">
+          <img
+            src={`https://storage.cloud.google.com/reboot-minty-storage/${imageList[currentImage]?.imgUrl}`}
+            alt="Board Image"
+            className="modal-img"
+          />
+        </Modal.Body>
+      </Modal>
+    </Container>
+  );
+}
+
+export default BoardDetail;
