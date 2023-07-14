9ea4d8c1208b467dac07cd4297c43eb4d03bfa90
권비 <62578653+bi-sz@users.noreply.github.com>
2023-07-14 16:57:11 +0900

    룰렛 하루 한 번 제한

--- src/main/resources/static/js/roulette.js
+++ src/main/resources/static/js/roulette.js
@@ -1,4 +1,4 @@
-  var rolLength = 6;
+    var rolLength = 6;
     var setNum;
     var hiddenInput = document.createElement("input");
     hiddenInput.className = "hidden-input";
@@ -37,48 +37,58 @@
       }, 50);
     };
 
+
     // 팝업 함수와 결과 저장 함수
-  const rLayerPopup = (num, point) => {
-    switch (num) {
-      case 1:
-        swal('당첨!!','100포인트 지급','success');
-        saveRouletteResult("당첨, 100포인트", 100);
-        break;
-      case 3:
-        swal('당첨!!','300포인트 지급','success');
-        saveRouletteResult("당첨, 300포인트", 300);
-        break;
-      case 5:
-        swal('당첨!!','1000포인트 지급','success');
-        saveRouletteResult("당첨, 1000포인트", 1000);
-        break;
-      default:
-         swal('꽝~','다음기회에..','error');
-        saveRouletteResult("꽝", 0);
-        break;
-    }
-  };
-
-  // 결과 저장 함수 (AJAX로 서버에 전송)
-  const saveRouletteResult = (result, point) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/roulette/save", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("X-CSRF-TOKEN", document.querySelector('meta[name="_csrf"]').getAttribute('content'));
-    xhr.setRequestHeader("X-CSRF-HEADER", document.querySelector('meta[name="_csrf_header"]').getAttribute('content'));
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-        console.log("Roulette result saved successfully");
-        console.log(`Points (${point}) saved successfully`);
-      } else if (xhr.readyState === XMLHttpRequest.DONE) {
-        console.log("Failed to save roulette result");
+    const rLayerPopup = (num, point) => {
+      var currentDate = new Date();
+      switch (num) {
+        case 1:
+          swal('당첨!!', '100포인트 지급', 'success').then(() => {
+            location.reload(); // Reload the page when swal is closed
+          });
+          saveRouletteResult("당첨, 100포인트", 100, currentDate);
+          break;
+        case 3:
+          swal('당첨!!', '300포인트 지급', 'success').then(() => {
+            location.reload(); // Reload the page when swal is closed
+          });
+          saveRouletteResult("당첨, 300포인트", 300, currentDate);
+          break;
+        case 5:
+          swal('당첨!!', '1000포인트 지급', 'success').then(() => {
+            location.reload(); // Reload the page when swal is closed
+          });
+          saveRouletteResult("당첨, 1000포인트", 1000, currentDate);
+          break;
+        default:
+          swal('꽝~', '다음기회에..', 'error').then(() => {
+            location.reload(); // Reload the page when swal is closed
+          });
+          saveRouletteResult("꽝", 0, currentDate);
+          break;
       }
     };
 
-    const data = JSON.stringify({ result, point });
-    xhr.send(data);
-  };
+    // 결과 저장 함수 (AJAX로 서버에 전송)
+    const saveRouletteResult = (result, point, currentDate) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", "/roulette/save", true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("X-CSRF-TOKEN", document.querySelector('meta[name="_csrf"]').getAttribute('content'));
+      xhr.setRequestHeader("X-CSRF-HEADER", document.querySelector('meta[name="_csrf_header"]').getAttribute('content'));
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+          console.log("Roulette result saved successfully");
+          console.log(`Points (${point}) saved successfully`);
+        } else if (xhr.readyState === XMLHttpRequest.DONE) {
+          console.log("Failed to save roulette result");
+        }
+      };
+
+      const data = JSON.stringify({ result, point, currentDate: currentDate.toISOString().split("T")[0] });
+      xhr.send(data);
+    };
 
     // 리셋 함수
     const rReset = (ele) => {
@@ -90,11 +100,17 @@
       }, 5500);
     };
 
+
     // 룰렛 이벤트 클릭 버튼
-document.addEventListener("click", function (e) {
+    document.addEventListener("click", function (e) {
       var target = e.target;
       if (target.tagName === "BUTTON") {
-        rRotate();
-        rReset(target);
+        var canSpin = document.querySelector(".rouletter").getAttribute("data-can-spin");
+        if (canSpin === "true") {
+          rRotate();
+          rReset(target);
+        } else {
+          swal("한번만 돌릴 수 있습니다");
+        }
       }
     });
