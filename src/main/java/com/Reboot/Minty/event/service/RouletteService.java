c3c425266a4500ccb24080ad96c433b28cad21f3
권비 <62578653+bi-sz@users.noreply.github.com>
2023-07-14 17:00:04 +0900

    룰렛 하루 한 번 제한 추가

--- src/main/java/com/Reboot/Minty/event/service/RouletteService.java
+++ src/main/java/com/Reboot/Minty/event/service/RouletteService.java
@@ -2,9 +2,13 @@ package com.Reboot.Minty.event.service;
 
 import com.Reboot.Minty.event.entity.Roulette;
 import com.Reboot.Minty.event.repository.RouletteRepository;
+import com.Reboot.Minty.member.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class RouletteService {
@@ -25,5 +29,13 @@ public class RouletteService {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public Roulette findByUserAndDate(User user, LocalDate currentDate) {
+        return rouletteRepository.findByUserAndDate(user, currentDate);
+    }
+
+    public List<Roulette> getRouletteByDate(LocalDate currentDate) {
+        return rouletteRepository.findByDate(currentDate);
     }
 }
