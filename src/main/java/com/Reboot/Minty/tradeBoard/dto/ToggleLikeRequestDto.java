d7beec3d7dfdafda25f0b82f163fee11ebd1b261
권비 <62578653+bi-sz@users.noreply.github.com>
2023-07-15 19:56:58 +0900

    Update ToggleLikeRequestDto.java

--- src/main/java/com/Reboot/Minty/tradeBoard/dto/ToggleLikeRequestDto.java
+++ src/main/java/com/Reboot/Minty/tradeBoard/dto/ToggleLikeRequestDto.java
@@ -1,18 +1,13 @@
-package com.Reboot.Minty.tradeBoard.repository;
+package com.Reboot.Minty.tradeBoard.dto;
 
-import com.Reboot.Minty.member.entity.User;
 import com.Reboot.Minty.tradeBoard.entity.TradeBoard;
-import com.Reboot.Minty.tradeBoard.entity.WishLike;
-import com.oracle.wls.shaded.org.apache.bcel.generic.LNEG;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.Optional;
-
-@Repository
-public interface WishLikeRepository extends JpaRepository<WishLike,Long> {
-
-    Optional<WishLike> findByTradeBoardAndUser(TradeBoard tradeBoard, User user);
-
-    WishLike findByUserAndTradeBoard(User user, TradeBoard boardId);
+@Getter
+@Setter
+public class ToggleLikeRequestDto {
+    private long id;
+    private Boolean isLiked;
 }
+
